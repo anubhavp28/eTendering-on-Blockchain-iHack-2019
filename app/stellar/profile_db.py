bcbbe8919c05c54140480b9b574348783f42b404
zeus0789 <abhishek.tiwari3507@gmail.com>
2019-01-19 22:41:08 +0530

    Updates mongodb script

--- app/stellar/profile_db.py
+++ app/stellar/profile_db.py
@@ -1,17 +1,10 @@
-import pymongo
-import datetime
 import requests
 from stellar_base.builder import Builder
 from stellar_base.address import Address
-from pymongo import MongoClient
 from stellar_base.utils import StellarMnemonic
 from stellar_base.keypair import Keypair
 
 
-client = MongoClient()
-
-db = client.users
-user_collection = db.user_collection
 
 #secret is the phrase so user does not has to enter both
 #public and private key
@@ -21,18 +14,10 @@ kp = Keypair.deterministic(secret, lang="english", index=1)
 publickey = kp.address().decode()
 seed = kp.seed().decode()
 
-profile = db.profile
-user = {"dept_id":"",
-		"dept_name":"",
-		"dept_city":"",
-		"dept_state":"",
-		"dept_accno":publickey,
-		"dept_secret":seed}
-	
 
 url = 'https://friendbot.stellar.org'
 r = requests.get(url, params={'addr':publickey})      
 print(publickey, seed)
-curr_user_id = profile.insert_one(user).inserted_id
+
 
 
